ee19eeca390e0cad496412ce205edea582605046
Damian Green <damian@gcoders.com>
2014-08-30 13:03:28 +0100

    3d piece added with d3

---
+++ sound-finder.core/Domain/Models/MusicItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sound_finder.core.Domain.Models
+{
+    public class MusicItem
+    {
+        public String Title { get; set; }
+    }
+
+}
---
+++ sound-finder.core/Domain/MusicService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sound_finder.core.Domain
+{
+    public class MusicService : IMusicService
+    {
+
+    }
+
+    internal interface IMusicService
+    {
+        
+    }
+}
